e276a65aa831addb786ac8aa40807d5f0ee90ded
khoitrandc <khoi.tran@deltacognition.com>
2025-04-21 21:53:45 +0700

    update domain

--- data_mining/src/main/java/com/example/gui/MainFrame.java
+++ data_mining/src/main/java/com/example/gui/MainFrame.java
@@ -8,9 +8,9 @@ public class MainFrame extends JFrame {
     public static void main(String[] args) throws Exception {
         MiningController controller = new MiningController();
         controller.runPipeline(
-            "/Users/Chanh/Data-Mining-Proj/data_mining/src/main/resources/data.csv",
-            "/Users/Chanh/Data-Mining-Proj/data_mining/src/main/resources/cleaned_dataset.arff",
-            "/Users/Chanh/Data-Mining-Proj/data_mining/src/main/resources/evaluation_report.txt"
+            "D:\\Project\\Data-Mining-Proj\\data_mining\\src\\main\\resources\\data.arff",
+            "D:\\Project\\Data-Mining-Proj\\data_mining\\src\\main\\resources\\cleaned_dataset.arff",
+            "D:\\Project\\Data-Mining-Proj\\data_mining\\src\\main\\resources\\evaluation_report.txt"
         );
     }
 }
